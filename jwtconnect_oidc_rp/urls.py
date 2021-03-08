cedae03f5726cb359190299a57bc3c4171468d8c
peppelinux <giuseppe.demarco@unical.it>
2021-03-08 12:36:30 +0100

    first test working view

--- jwtconnect_oidc_rp/urls.py
+++ jwtconnect_oidc_rp/urls.py
@@ -5,4 +5,4 @@ from . views import oidc_rp_begin
 
 
 urlpatterns = []
-urlpatterns += path('sp/begin', oidc_rp_begin, name='jwtconnect_oidc_rp_begin'),
+urlpatterns += path('rp/begin', oidc_rp_begin, name='jwtconnect_oidc_rp_begin'),
--- jwtconnect_oidc_rp/views.py
+++ jwtconnect_oidc_rp/views.py
@@ -6,10 +6,23 @@ from django.http import (Http404,
 from django.shortcuts import render
 from jwtconnect_oidc_rp.utils import get_rph
 
+from . exceptions import MisconfiguredClientIssuer
+
 
 logger = logging.getLogger(__name__)
 
 
 def oidc_rp_begin(request):
     rph = get_rph()
+
+    available_issuers = settings.JWTCONN_OIDC_RP['clients']
+    available_issuers_len = len(available_issuers)
+    if available_issuers_len > 1:
+        issuer_id = request.GET.get('issuer_id') 
+    elif available_issuers_len == 1:
+        issuer_id = available_issuers.keys()[0]
+    else:
+        raise MisconfiguredClientIssuer('No available issuers found')
+
+    info = rph.begin(issuer_id)
     return HttpResponseRedirect(info['url'])
